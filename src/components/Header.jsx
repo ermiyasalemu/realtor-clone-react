529a5c65ab6cfcb4230b4fc558a24a3974e8fe7b
ermiyas alemu <ermiyasalemu54@gmail.com>
2024-10-24 17:42:16 +0300

    create private route and custom hook for protection the profile page and add the logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,12 @@ export default function Header() {
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
+    } else {
+      return false;
     }
   }
+
+
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -22,28 +26,31 @@ export default function Header() {
         <div>
           <ul className="flex space-x-10">
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-            border-b-transparent ${
-              pathMatchRoute("/") && "text-black border-b-red-400"
-            }`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMatchRoute("/")
+                  ? "text-black border-b-4 border-b-red-400"
+                  : "text-gray-400 border-b-3 border-b-transparent"
+              }`}
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-            border-b-transparent ${
-              pathMatchRoute("/offers") && "text-black border-b-red-400"
-            }`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMatchRoute("/offers")
+                  ? "text-black border-b-4 border-b-red-400"
+                  : "text-gray-400 border-b-3 border-b-transparent"
+              }`}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-            border-b-transparent ${
-              pathMatchRoute("/sign-in") && "text-black border-b-red-400"
-            }`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMatchRoute("/sign-in")
+                  ? "text-black border-b-4 border-b-red-400"
+                  : "text-gray-400 border-b-3 border-b-transparent"
+              }`}
               onClick={() => navigate("/sign-in")}
             >
               Sign in
