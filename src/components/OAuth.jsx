d5aae777c0829305188c6f07998466d76a504e50
ermiyas alemu <ermiyasalemu54@gmail.com>
2024-10-24 07:58:01 +0300

    complete the oath functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,44 @@
-import { FcGoogle } from "react-icons/fc"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
 export default function OAuth() {
+  const navigate = useNavigate();
+
+async function onGoogleClick() {
+
+  try {
+    
+    const auth = getAuth(); 
+    const provider = new GoogleAuthProvider()
+    const  result = await signInWithPopup(auth, provider)
+    const user = result.user
+    // check for the user exist in db 
+    const docRef = doc(db, "users", user.uid)
+    const docSnap = await getDoc(docRef)
+
+    if(!docSnap.exists()){
+      await setDoc(docRef, {
+        name: user.displayName, 
+        email: user.email, 
+        timestamp: serverTimestamp(),
+
+      })
+    } // end of if
+    
+    navigate("/")
+
+  } catch (error) {
+    toast.error("Could not authorize with Google !!")
+
+  }
+
+}
   return (
     <div>
-     <button className="flex item-center 
+     <button type="button" onClick={onGoogleClick} className="flex item-center 
      justify-center
       w-full bg-red-700 text-white
       px-7 py-3 uppercase
