2612685958e21509da18c9e012917ea5a8a3f992
ermiyas alemu <ermiyasalemu54@gmail.com>
2024-10-28 14:57:12 +0300

    create listing page and use swiper to add the image slider

--- src/App.js
+++ src/App.js
@@ -1,59 +1,63 @@
-import "./index.css"
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-import Signup from "./pages/SignUp"; 
+import Signup from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute.";
-import ForgotPassword from "./pages/ForgotPassword"
-import Offers from "./pages/Offers"
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
+
 
 function App() {
   return (
-    <> 
-  <Router> 
-    <Header />
-    <Routes> 
-      <Route path="/" element={<Home/>}/>
-        <Route path="/profile" element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />}/>
-        </Route>
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<Signup/>}/>
-      <Route path="/forgot-password" element={<ForgotPassword/>}/>
-      <Route path="/offers" element={<Offers/>}/>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
 
-    <Route path="create-listing" element={<PrivateRoute />}>
-    <Route path="/create-listing" element={<CreateListing/>}/>
-    </Route>
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
 
-    <Route path="edit-listing" element={<PrivateRoute />}>
-    <Route path="/edit-listing/:listingId" element={<EditListing/>}/>
-    </Route>
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
 
-
-      </Routes>
-    </Router>
-    <ToastContainer 
-    position="bottom-center"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme="dark"
-    />
-    
-    </> 
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   );
 }
 
